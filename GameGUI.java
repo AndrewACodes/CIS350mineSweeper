6d92d29926f5433d667c40f4694247c5d4a01940
Ryley Rawlings <rawliryl@mail.gvsu.edu>
2022-02-28 19:23:09 -0500

    Finished checkstyle changes

--- GameGUI.java
+++ GameGUI.java
@@ -1,9 +1,18 @@
 package CIS350mineSweeper;
+
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
-public class GameGUI extends JFrame implements ActionListener, MouseListener{
+
+/**
+ * The GameGUi.java class is a fully functioning GUI used to play the game MineSweeper.
+ * This includes the creation of the GUI itself, as well as methods for winning, losing, 
+ * and revealing tiles.
+ * @author Ryley Rawlings, Kaden Wende, and Andrew Arent
+ * @version 1.0
+ */
+
+public class GameGUI extends JFrame implements ActionListener{
 
 	JButton restartButton = new JButton("Reset");
 	JButton giveUpButton = new JButton("Give Up");
@@ -12,6 +21,13 @@ public class GameGUI extends JFrame implements ActionListener, MouseListener{
 	JButton[][] buttonGrid;
 	Minesweeper game;
 
+	/**
+	 * Constructs a new GameGUI with a specified board size and number of mines.
+	 * Creates all GUI components and places them on  a JFrame
+	 * @param boardSize the number of tiles each side of the game board should have.
+	 * @param numMines the number of mines that should be placed on the game board
+	 */
+	
 	public GameGUI(int boardSize, int numMines) {
 		super("MinesweeperGameGUI");
 
@@ -19,7 +35,7 @@ public class GameGUI extends JFrame implements ActionListener, MouseListener{
 		game.generateMines();
 		game.generateTileValues();
 
-		setSize(800,800);
+		setSize(800, 800);
 		setLayout(new BorderLayout()); 
 		restartButton.addActionListener(this);
 		giveUpButton.addActionListener(this);
@@ -28,8 +44,8 @@ public class GameGUI extends JFrame implements ActionListener, MouseListener{
 		add(bottomPanel, BorderLayout.NORTH);
 
 		buttonGrid = new JButton[boardSize][boardSize];
-		for(int i = 0; i < boardSize; i++) {
-			for(int j = 0; j < boardSize; j++) {
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
 				buttonGrid[i][j] = new JButton();
 				buttonGrid[i][j].addActionListener(this);
 				tilePanel.add(buttonGrid[i][j]);
@@ -41,15 +57,14 @@ public class GameGUI extends JFrame implements ActionListener, MouseListener{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-
-
 	}
-	
+
 	private void checkIfWin() {
 		int remainingClearTiles = 0;
-		for(int i = 0; i < game.boardSize; i++) {
-			for(int j = 0; j < game.boardSize; j++) {
-				if(buttonGrid[i][j].isEnabled() == true && game.getTileValue(i, j) != Minesweeper.MINE_VALUE) remainingClearTiles++;
+		for (int i = 0; i < game.boardSize; i++) {
+			for (int j = 0; j < game.boardSize; j++) {
+				if (buttonGrid[i][j].isEnabled() == true 
+						&& game.getTileValue(i, j) != Minesweeper.MINE_VALUE) remainingClearTiles++;
 			}
 		}
 		if (remainingClearTiles == 0) {
@@ -58,8 +73,8 @@ public class GameGUI extends JFrame implements ActionListener, MouseListener{
 	}
 
 	private void restartBoard() {
-		for(int i = 0; i < game.boardSize; i++) {
-			for(int j = 0; j < game.boardSize; j++) {
+		for (int i = 0; i < game.boardSize; i++) {
+			for (int j = 0; j < game.boardSize; j++) {
 				buttonGrid[i][j].setText("");
 				buttonGrid[i][j].setEnabled(true); 
 			}
@@ -69,74 +84,100 @@ public class GameGUI extends JFrame implements ActionListener, MouseListener{
 	private void reveal(int x, int y) {
 		buttonGrid[x][y].setText(""+game.getTileValue(x, y));
 		buttonGrid[x][y].setEnabled(false);
-		if(game.getTileValue(x, y) == 0) {
-			if (x > 0) { //if tile is not a leftmost tile in a row
-				if(game.getTileValue(x-1, y) != Minesweeper.MINE_VALUE) {
-					buttonGrid[x-1][y].setText(""+game.getTileValue(x-1, y)); //tile to the left of [x][y]
-					if(game.getTileValue(x-1, y) == 0 && buttonGrid[x-1][y].isEnabled() != false){
+		if (game.getTileValue(x, y) == 0) {
+			//if tile is not a leftmost tile in a row
+			if (x > 0) { 
+				if (game.getTileValue(x-1, y) != Minesweeper.MINE_VALUE) {
+					//tile to the left of [x][y]
+					buttonGrid[x-1][y].setText(""+game.getTileValue(x-1, y)); 
+					if (game.getTileValue(x-1, y) == 0 
+							&& buttonGrid[x-1][y].isEnabled() != false){
 						reveal(x-1, y);
 					}
 					buttonGrid[x-1][y].setEnabled(false);
 				}
-				if(y > 0){ //if tile is not an uppermost tile in a column
-					if(game.getTileValue(x-1, y-1) != Minesweeper.MINE_VALUE) {
-						buttonGrid[x-1][y-1].setText("" + game.getTileValue(x-1, y-1));//tile to the upper left of [x][y]
-						if(game.getTileValue(x-1, y-1) == 0 && buttonGrid[x-1][y-1].isEnabled() != false){
+				//if tile is not an uppermost tile in a column
+				if (y > 0){ 
+					if (game.getTileValue(x-1, y-1) != Minesweeper.MINE_VALUE) {
+						//tile to the upper left of [x][y]
+						buttonGrid[x-1][y-1].setText("" 
+						+ game.getTileValue(x-1, y-1)); 
+						if (game.getTileValue(x-1, y-1) == 0 
+								&& buttonGrid[x-1][y-1].isEnabled() != false){
 							reveal(x-1, y-1);
 						}
 						buttonGrid[x-1][y-1].setEnabled(false);
 					}
 				} 
-				if (y < game.boardSize -1) { //if tile is not the bottommost tile in a column
-					if(game.getTileValue(x-1, y) != Minesweeper.MINE_VALUE) {
-						buttonGrid[x-1][y+1].setText("" + game.getTileValue(x-1, y+1)); //tile to the lower left of [x][y]
-						if(game.getTileValue(x-1, y+1) == 0 && buttonGrid[x-1][y+1].isEnabled() != false){
+				//if tile is not the bottommost tile in a column
+				if (y < game.boardSize -1) { 
+					if (game.getTileValue(x-1, y) 
+							!= Minesweeper.MINE_VALUE) {
+						//tile to the lower left of [x][y]
+						buttonGrid[x-1][y+1].setText("" 
+						+ game.getTileValue(x-1, y+1)); 
+						if (game.getTileValue(x-1, y+1) == 0 
+								&& buttonGrid[x-1][y+1].isEnabled() != false){
 							reveal(x-1, y+1);
 						}
 						buttonGrid[x-1][y+1].setEnabled(false);
 					}
 				}
 			}
-			if(x < game.boardSize -1) { //if tile is not rightmost tile in a row
-				if(game.getTileValue(x+1, y) != Minesweeper.MINE_VALUE) {
-					buttonGrid[x+1][y].setText(""+game.getTileValue(x+1, y)); //tile to the right of [x][y]
-					if(game.getTileValue(x+1, y) == 0 && buttonGrid[x+1][y].isEnabled() != false){
+			//if tile is not rightmost tile in a row
+			if (x < game.boardSize -1) { 
+				if (game.getTileValue(x+1, y) != Minesweeper.MINE_VALUE) {
+					//tile to the right of [x][y]
+					buttonGrid[x+1][y].setText(""+game.getTileValue(x+1, y)); 
+					if (game.getTileValue(x+1, y) == 0 
+							&& buttonGrid[x+1][y].isEnabled() != false){
 						reveal(x+1, y);
 					}
 					buttonGrid[x+1][y].setEnabled(false);
 				}
-				if(y > 0) {//if tile is not uppermost tile in a column
-					if(game.getTileValue(x+1, y-1) != Minesweeper.MINE_VALUE) {
-						buttonGrid[x+1][y-1].setText("" + game.getTileValue(x+1, y-1)); //tile to the upper right of [x][y]
-						if(game.getTileValue(x+1, y-1) == 0 && buttonGrid[x+1][y-1].isEnabled() != false){
+				//if tile is not uppermost tile in a column
+				if (y > 0) {
+					if (game.getTileValue(x+1, y-1) != Minesweeper.MINE_VALUE) {
+						//tile to the upper right of [x][y]
+						buttonGrid[x+1][y-1].setText("" 
+						+ game.getTileValue(x+1, y-1)); 
+						if (game.getTileValue(x+1, y-1) == 0 
+								&& buttonGrid[x+1][y-1].isEnabled() != false){
 							reveal(x+1, y-1);
 						}
 						buttonGrid[x+1][y-1].setEnabled(false);
 					}
 				}
-				if (y < game.boardSize -1) { //if tile is not bottommost tile in a column
-					if(game.getTileValue(x+1, y+1) != Minesweeper.MINE_VALUE) {
-						buttonGrid[x+1][y+1].setText("" + game.getTileValue(x+1, y+1)); //tile to the lower right of [x][y]
-						if(game.getTileValue(x+1, y+1) == 0 && buttonGrid[x+1][y+1].isEnabled() != false){
+				//if tile is not bottommost tile in a column
+				if (y < game.boardSize -1) { 
+					if (game.getTileValue(x+1, y+1) != Minesweeper.MINE_VALUE) {
+						//tile to the lower right of [x][y]
+						buttonGrid[x+1][y+1].setText("" 
+						+ game.getTileValue(x+1, y+1)); 
+						if (game.getTileValue(x+1, y+1) == 0 
+								&& buttonGrid[x+1][y+1].isEnabled() != false){
 							reveal(x+1, y+1);
 						}
 						buttonGrid[x+1][y+1].setEnabled(false);
 					}
 				}
 			}
-			if(y > 0) { //if tile is not uppermost tile in a column
-				if(game.getTileValue(x, y-1) != Minesweeper.MINE_VALUE) {
-					buttonGrid[x][y-1].setText(""+game.getTileValue(x,y-1)); //tile above [x][y]
-					if(game.getTileValue(x, y-1) == 0 && buttonGrid[x][y-1].isEnabled() != false){
+			//if tile is not uppermost tile in a column
+			if (y > 0) { 
+				if (game.getTileValue(x, y-1) != Minesweeper.MINE_VALUE) {
+					//tile above [x][y]
+					buttonGrid[x][y-1].setText(""+game.getTileValue(x, y-1)); 
+					if (game.getTileValue(x, y-1) == 0 && buttonGrid[x][y-1].isEnabled() != false){
 						reveal(x, y-1);
 					}
 					buttonGrid[x][y-1].setEnabled(false);
 				}
 			}
-			if(y < game.boardSize -1) {//if tile is not bottommost tile in a column
-				if(game.getTileValue(x, y+1) != Minesweeper.MINE_VALUE) {
+			//if tile is not bottommost tile in a column
+			if (y < game.boardSize -1) {
+				if (game.getTileValue(x, y+1) != Minesweeper.MINE_VALUE) {
 					buttonGrid[x][y+1].setText(""+game.getTileValue(x, y+1));
-					if(game.getTileValue(x, y+1) == 0 && buttonGrid[x][y+1].isEnabled() != false){
+					if (game.getTileValue(x, y+1) == 0 && buttonGrid[x][y+1].isEnabled() != false){
 						reveal(x, y+1);
 					}
 					buttonGrid[x][y+1].setEnabled(false);
@@ -145,75 +186,45 @@ public class GameGUI extends JFrame implements ActionListener, MouseListener{
 		}
 	}
 
-		public void gameOver() {
-			for(int i = 0; i < game.boardSize; i++) {
-				for(int j = 0; j < game.boardSize; j++) {
-					buttonGrid[i][j].setText("" + game.getTileValue(i, j));
-					buttonGrid[i][j].setEnabled(false); 
-				}
+	private void gameOver() {
+		for (int i = 0; i < game.boardSize; i++) {
+			for (int j = 0; j < game.boardSize; j++) {
+				buttonGrid[i][j].setText("" + game.getTileValue(i, j));
+				buttonGrid[i][j].setEnabled(false); 
 			}
 		}
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(e.getSource() == restartButton) {
-				restartBoard();
-				game.restart();
-			}
-			else if(e.getSource() == giveUpButton) {
-				gameOver();
-			}
-			else {
-				for(int i = 0; i < game.boardSize; i++) {
-					for(int j = 0; j < game.boardSize; j++) {
-						if(e.getSource() == buttonGrid[i][j]) {
-							if(game.getTileValue(i, j) != Minesweeper.MINE_VALUE) {
-								reveal(i, j);
-								checkIfWin();
-							}else {
-								gameOver();
-								JOptionPane.showMessageDialog(null, "You clicked on a mine. Game over.");
-							}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == restartButton) {
+			restartBoard();
+			game.restart();
+		} else if (e.getSource() == giveUpButton) {
+			gameOver();
+		} else {
+			for (int i = 0; i < game.boardSize; i++) {
+				for (int j = 0; j < game.boardSize; j++) {
+					if (e.getSource() == buttonGrid[i][j]) {
+						if (game.getTileValue(i, j) 
+								!= Minesweeper.MINE_VALUE) {
+							reveal(i, j);
+							checkIfWin();
+						} else {
+							gameOver();
+							JOptionPane.showMessageDialog
+							(null, "You clicked on a mine. Game over.");
 						}
 					}
 				}
 			}
-
-
 		}
 
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-
-		}
-
-		public static void main(String[] args) {
-			GameGUI GUI = new GameGUI(15, 25);
-		}
 
 	}
+
+	public static void main(String[] args) {
+		new GameGUI(15, 25);
+	}
+
+}
--- MinesweeperUnitTests.java
+++ MinesweeperUnitTests.java
@@ -8,15 +8,15 @@ import org.junit.jupiter.api.Test;
 public class MinesweeperUnitTests {
 	private Minesweeper minesweeper;
 	private int[][] valueArray;
-	
+
 	@BeforeEach
 	void createNewMinesweeper() {
-		minesweeper = new Minesweeper(15,15);
+		minesweeper = new Minesweeper(15, 15);
 		minesweeper.generateMines();
 		minesweeper.generateTileValues();
 		valueArray = minesweeper.getValueArray();
 	}
-	
+
 	@Test
 	void minesweeperConstructorTest() {
 		assertEquals(15, minesweeper.boardSize);
@@ -24,7 +24,7 @@ public class MinesweeperUnitTests {
 		assertEquals(15, valueArray.length);
 		assertEquals(15, valueArray[0].length);
 	}
-	
+
 	@Test
 	void getTileValueTest() {
 		for(int i = 0; i < minesweeper.boardSize; i++) {
@@ -40,26 +40,26 @@ public class MinesweeperUnitTests {
 			for(int i = 0; i < minesweeper.boardSize; i++) {
 				for(int j = 0; j < minesweeper.boardSize; j++) {
 					//tiles cannot have 9 or more surrounding bombs, or negative surrounding bombs; -1 is a bomb, however
-					assertTrue(minesweeper.getTileValue(i,j) < 9 && minesweeper.getTileValue(i,j) > -2);				
+					assertTrue(minesweeper.getTileValue(i, j) < 9 && minesweeper.getTileValue(i, j) > -2);				
 				}
 			}
-		minesweeper = new Minesweeper(15,15);
-		minesweeper.generateMines();
-		minesweeper.generateTileValues();
+			minesweeper = new Minesweeper(15, 15);
+			minesweeper.generateMines();
+			minesweeper.generateTileValues();
 		}
 	}
-	
+
 	@Test
 	void generateMinesCorrectNumMines() {
 		int numMinesOnBoard = 0;
 		for(int i = 0; i < minesweeper.boardSize; i++) {
 			for(int j = 0; j < minesweeper.boardSize; j++) {
-				if(minesweeper.getTileValue(i,j) == Minesweeper.MINE_VALUE) numMinesOnBoard++;
+				if(minesweeper.getTileValue(i, j) == Minesweeper.MINE_VALUE) numMinesOnBoard++;
 			}
 		}
 		assertEquals(minesweeper.numMines, numMinesOnBoard);
 	}
-	
+
 	@Test
 	void restartGivesDifferentBoard() {
 		minesweeper.restart();
